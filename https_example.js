511fcdef7dd9b0d2cd3c4edd298e5e7737fe9ebd
Sahanah Ganesh <sahanah.ganesh@gmail.com>
2019-04-08 02:09:49 +0000

    trying to get incoming msg

--- https_example.js
+++ https_example.js
@@ -8,12 +8,13 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Respose: ', response);
 }
 
 console.log("I'm about to make the request!");
 
 https.request(options, callback).end();
 
-console.log("I've made the request!")
+console.log("I've made the request!");
